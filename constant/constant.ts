06e1db8e15f3496a63436af26f3703db2435a9c0
yikwing <hubeieasy@gmail.com>
2023-04-28 15:56:41 +0800

    feat: 获取列表

--- constant/constant.ts
+++ constant/constant.ts
@@ -1,2 +1,2 @@
-// export const base_url = "http://138.2.227.220:8000"
-export const base_url = "http://localhost:8000"
+export const base_url = "http://138.2.227.220:8000"
+// export const base_url = "http://localhost:8000"
